ce3a1f511a6588fb3ae863ef6c6643147f8b5756
Feliperb03 <112295083+Feliperb03@users.noreply.github.com>
2026-05-01 16:57:29 -0400

    Update .zsh

--- .zsh
+++ .zsh
@@ -1,4 +1,4 @@
-#Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
@@ -8,7 +8,7 @@ fi
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME=""powerlevel10k/powerlevel10k""
 
-plugins=(git)
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting copypath copyfile web-search)
 
 source $ZSH/oh-my-zsh.sh
 
